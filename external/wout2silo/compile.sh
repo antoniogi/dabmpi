16a24cca0a021e1acc203d385ae31761db07ee36
antoniogi <antoniogi@gmail.com>
2015-12-12 10:33:46 -0600

    Removed unnecessary files

--- external/wout2silo/compile.sh
+++
@@ -1,3 +0,0 @@
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/u5494/software/tools/hdf5/lib/
-export HDF5LIB=/home/u5494/software/tools/hdf5/lib/
-icc main.cpp -I/home/u5494/software/tools/hdf5/include -I/home/u5494/software/tools/silo/include/ -L$HDF5LIB -L/home/u5494/software/tools/silo/lib/ -o vmec2silodirecto -lhdf5 -lsiloh5 -lm /usr/lib64/libstdc++.so.5
