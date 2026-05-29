d848c7077eaadc206165182916f6054294427704
Antonio Gomez <antoniogi@gmail.com>
2026-05-29 16:37:15 -0700

    Minor modernization

--- src/solvers/SolverBase.py
+++ src/solvers/SolverBase.py
@@ -17,7 +17,7 @@
 #   limitations under the License.                                          #
 #############################################################################
 
-__author__ = ' AUTHORS:     Antonio Gomez (antonio.gomez@csiro.au)'
+__author__ = ' AUTHORS:     Antonio Gomez'
 
 
 __version__ = ' REVISION:   2.0  -  28-05-2026'
@@ -37,30 +37,32 @@ from solution.SolutionsQueue import SolutionsQueue
 
 class SolverBase(ABC):
     """
-    Abstract base class defining the lifecycle protocol for processing 
-    distributed solutions via communication queues.
+    Abstract base class for all solver implementations.
+
+    Defines the solver lifecycle (initialize, solve, finish) and
+    provides common solution queues used for distributed execution.
     """
     def __init__(self, runtime: GlobalRuntime, comms: GlobalComms):
-        self._runtime = runtime
-        self._comms = comms
-        self._finishedSolutions = SolutionsQueue(
+        self._runtime: GlobalRuntime = runtime
+        self._comms: GlobalComms = comms
+        self._finishedSolutions: SolutionsQueue = SolutionsQueue(
             runtime, comms, "finished.queue", writeToFile=True, isPriority=True
             )
-        self._pendingSolutions = SolutionsQueue(
+        self._pendingSolutions: SolutionsQueue = SolutionsQueue(
             runtime, comms, "pending.queue", writeToFile=False
             )
-        self._topSolutions = SolutionsQueue(
+        self._topSolutions: SolutionsQueue = SolutionsQueue(
             runtime, comms, "top.queue", writeToFile=False, isPriority=True
             )
 
     @abstractmethod
-    def initialize(self):
-        pass
+    def initialize(self) -> None:
+        ...
     
     @abstractmethod
-    def solve(self):
-        pass
+    def solve(self) -> None:
+        ...
 
     @abstractmethod
-    def finish(self):
-        pass
+    def finish(self) -> None:
+        ...
